8686d0c797595291a0a2264e5d28b624011ef303
skrewbar <skurua1244@gmail.com>
2025-06-26 21:50:22 +0900

    🎨 트라이 재작성

--- Data Structures/trie.cpp
+++ Data Structures/trie.cpp
@@ -1,40 +1,49 @@
-#include <bits/stdc++.h>
+#include <map>
+#include <string>
 
-struct TrieNode {
-    bool end;
-    TrieNode *childs[26];
+class Trie {
+   public:
+    Trie() { root = new TrieNode; }
 
-    TrieNode() {
-        end = false;
-        for (int i = 0 ; i < 26 ; i++) childs[i] = nullptr;
-    }
+    void insert(const char s[]) { root->insert(s); }
+    void insert(const std::string& s) { root->insert(s.c_str()); }
 
+    bool contains(const char s[]) { return root->find(s); }
+    bool contains(const std::string& s) { return root->find(s.c_str()); }
 
-    void insert(char *str) {
-        if (*str == '\0') {
-            end = true;
-            return;
+   private:
+    struct TrieNode {
+        std::map<char, TrieNode*> child;
+        bool end;
+
+        TrieNode() : end(false) {}
+        ~TrieNode() {
+            for (auto& [_, ptr] : child)
+                delete ptr;
         }
 
-        int cur = *str - 'A';
-        if (childs[cur] == nullptr) childs[cur] = new TrieNode();
-        childs[cur]->insert(str + 1);
-    }
+        void insert(const char* str) {
+            if (*str == '\0') {
+                end = true;
+                return;
+            }
 
-
-    bool find(char *str) {
-        if (*str == '\0') {
-            if (end) return true;
-            else return false;
+            char next = *str;
+            if (not child.contains(next))
+                child[next] = new TrieNode;
+            child[next]->insert(str + 1);
         }
 
-        int cur = *str - 'A';
-        if (childs[cur] == nullptr) return false;
-        else return childs[cur]->find(str + 1);
-    }
+        bool find(const char* str) {
+            if (*str == '\0')
+                return end;
+
+            char next = *str;
+            if (not child.contains(next))
+                return false;
+            return child[next]->find(str + 1);
+        }
+    };
+
+    TrieNode* root;
 };
-
-
-int main() {
-    return 0;
-}
