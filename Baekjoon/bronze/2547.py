afbdaa22b7f29fd09d39fe48ebf3d2e97bc868de
skrewbar <skurua1244@gmail.com>
2025-12-16 19:04:42 +0900

    ✅ 백준 2547번 해결

---
+++ Baekjoon/bronze/2547.py
@@ -0,0 +1,9 @@
+def main():
+    for _ in range(int(input())):
+        input()
+        N = int(input())
+
+        print("YES" if sum([int(input()) for _ in range(N)]) % N == 0 else "NO")
+
+
+main()
