98e4c7c64aa22e655c66bfacb65bd4389cbd8d5f
skrewbar <skurua1244@gmail.com>
2026-03-18 09:05:32 +0900

    📝 디닉 알고리즘 재작성

--- Algorithms/Dinic.cpp
+++ Algorithms/Dinic.cpp
@@ -1,19 +1,70 @@
-#include <algorithm>
 #include <queue>
 #include <vector>
 using namespace std;
 
 constexpr int INF = 1e9;
 
-class FlowNetwork {
-   public:
-    FlowNetwork(size_t size, int source, int sink)
-        : source(source), sink(sink), adj(size), curEdge(size), level(size) {}
+struct FlowNetwork {
+    struct Edge {
+        int to, capacity, flow, rev;
+    };
+
+    int source, sink;
+    vector<vector<Edge>> adj;
+    vector<int> level, curEdge;
+
+    FlowNetwork(int n, int _source, int _sink)
+        : source(_source), sink(_sink), adj(n), level(n), curEdge(n) {}
 
     void addEdge(int from, int to, int capacity) {
-        Edge *edge = new Edge(to, capacity), *rev = new Edge(from, 0);
-        edge->rev = rev, rev->rev = edge;
-        adj[from].push_back(edge), adj[to].push_back(rev);
+        adj[from].emplace_back(to, capacity, 0, adj[to].size());
+        adj[to].emplace_back(from, 0, 0, adj[from].size() - 1);
+    }
+
+    int pushFlow(int cur, int flow) {
+        if (cur == sink)
+            return flow;
+
+        for (int& i = curEdge[cur]; i < adj[cur].size(); i++) {
+            auto& [to, capacity, edgeFlow, rev] = adj[cur][i];
+
+            if (level[to] != level[cur] + 1 or capacity - edgeFlow == 0)
+                continue;
+
+            int bottleneck = pushFlow(to, min(flow, capacity - edgeFlow));
+            if (bottleneck) {
+                edgeFlow += bottleneck;
+                adj[to][rev].flow -= bottleneck;
+
+                return bottleneck;
+            }
+        }
+
+        return 0;
+    }
+
+    bool buildLevelGraph() {
+        ranges::fill(level, -1);
+
+        queue<int> q;
+
+        level[source] = 0;
+        q.push(source);
+
+        while (not q.empty()) {
+            int cur = q.front();
+            q.pop();
+
+            for (const auto& [to, capacity, flow, _] : adj[cur]) {
+                if (level[to] != -1 or capacity - flow == 0)
+                    continue;
+
+                level[to] = level[cur] + 1;
+                q.push(to);
+            }
+        }
+
+        return level[sink] != -1;
     }
 
     int getMaxFlow() {
@@ -27,66 +78,5 @@ class FlowNetwork {
         }
 
         return maxFlow;
-    }
-
-   private:
-    struct Edge {
-        Edge* rev;
-        int to, capacity, flow;
-        Edge(int to, int capacity) : to(to), capacity(capacity), flow(0) {}
-
-        void pushFlow(int f) {
-            flow += f;
-            rev->flow -= f;
-        }
-
-        int residualCapacity() { return capacity - flow; }
-    };
-
-    int source, sink;
-    vector<vector<Edge*>> adj;
-    vector<int> curEdge, level;
-
-    bool buildLevelGraph() {
-        ranges::fill(level, -1);
-        level[source] = 0;
-        queue<int> q;
-        q.push(source);
-
-        while (not q.empty()) {
-            int now = q.front();
-            q.pop();
-
-            for (Edge* edge : adj[now]) {
-                if (level[edge->to] != -1 or edge->residualCapacity() == 0)
-                    continue;
-
-                level[edge->to] = level[now] + 1;
-                q.push(edge->to);
-            }
-        }
-
-        return level[sink] != -1;
-    }
-
-    int pushFlow(int now, int flow) {
-        if (now == sink)
-            return flow;
-
-        for (int& i = curEdge[now]; i < (int)adj[now].size(); i++) {
-            Edge* edge = adj[now][i];
-
-            if (level[edge->to] != level[now] + 1 or
-                edge->residualCapacity() == 0)
-                continue;
-
-            int f = pushFlow(edge->to, min(flow, edge->residualCapacity()));
-            if (f) {
-                edge->pushFlow(f);
-                return f;
-            }
-        }
-
-        return 0;
     }
 };
